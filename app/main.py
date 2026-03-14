15fd004f6e34d4a843bce108215cc77bee5ea967
Petro Prykhodko <petro.prykhodko2004@gmail.com>
2026-03-14 13:48:01 +0100

    solution 1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3:
+        return
+
+    cmd, fin, fout = parts
+
+    if cmd != "cp":
+        return
+
+    if fin == fout:
+        return
+
+    if not os.path.exists(fin):
+        return
+
+    with open(fin, "r") as f1, open(fout, "w") as f2:
+        f2.write(f1.read())
+
+
+if __name__ == "__main__":
+    copy_file("cp file.txt new_file.txt")
